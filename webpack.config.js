361cad5e57c2eceb55f984d1abfe36de54e0f020
Pedro Araujo <pharaujo@uolinc.com>
2017-05-29 15:43:03 -0300

    fix ´self is not defined´ fetch

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,16 @@ module.exports = {
     libraryTarget: 'umd',
     library: 'wobbuffetch'
   },
+  externals: [
+    {
+      'isomorphic-fetch': {
+        root: 'isomorphic-fetch',
+        commonjs2: 'isomorphic-fetch',
+        commonjs: 'isomorphic-fetch',
+        amd: 'isomorphic-fetch'
+      }
+    }
+  ],
   resolve: {
     modules: [path.resolve(process.cwd(), 'src'), 'node_modules'],
     extensions: ['.js']
